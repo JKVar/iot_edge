25d67ce8ace5fb285e5f9cbaf4d5348e94937818
HVMetal <fonodizsolt20@gmail.com>
2025-10-15 21:49:46 +0300

    Light sensor added. Automatic/Manual logic added. Boy, do these functions need cleanups. Im hungry for spaghetti now!

--- edge/edge.ino
+++ edge/edge.ino
@@ -1,6 +1,8 @@
 #include "DHT.h"
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
 #include <DS3231.h>
 #define DHTPIN A3     // Digital pin connected to the DHT sensor
 
@@ -13,6 +15,7 @@
 #endif
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
 
 DHT dht(DHTPIN, DHTTYPE);
 RTClib myRTC;
@@ -77,8 +80,79 @@ byte gradeChar[] = {
 
 int buttonOldState, buttonState;
 
-void checkSensors() {
+bool isManual; // false - automatic, true - manual
+int lastButtonChangeTimestamp = 0;
+const int automaticManualChangeDelay = 300; // in seconds
+bool isItDay; // false - it's night (who would have guessed), true - it's day
+bool isShutterUp = true;
 
+short luminosity; // <= 0 - too bright, 1 - okay, >= 2 - too dark
+const float normalLightThreshold = 100;
+const float tooBrightThreshold = 1000;
+bool isLightSensorDetected = true;
+
+void checkSensors() {
+  
+}
+
+short assessLuminosity(float light) {
+  if (light < normalLightThreshold) {
+    luminosity = 2;
+  } else if (light < tooBrightThreshold) {
+    luminosity = 1;
+  } else {
+    luminosity = 0;
+  }
+}
+
+void checkDayTime(uint8_t hour) {
+  if (hour >= 8 && hour <= 20) {
+    isItDay = true;
+  } else {
+    isItDay = false;
+  }
+}
+
+void canSwitchMode(DateTime currentDate) {
+  int unixT = currentDate.unixtime();
+  int timeDiff = abs(unixT - lastButtonChangeTimestamp);
+
+  if (timeDiff >= automaticManualChangeDelay) {
+    isManual = false;
+  }
+}
+
+void changeShutters(bool state) {
+  // TODO: make the engine go brrrr
+  isShutterUp = state;
+}
+
+void automaticModeLogic(DateTime currentDate) {
+  if (!isManual) {
+    if (isItDay) {
+      if (luminosity <= 0 && isShutterUp) {
+        changeShutters(false);
+      }
+      if (luminosity >= 2 && !isShutterUp) {
+        changeShutters(true);
+      }
+    } else {
+      if (isShutterUp) {
+        changeShutters(false);
+      }
+    }
+  } else {
+    canSwitchMode(currentDate);
+  }
+}
+
+void configureLightSensor() {
+  if (tsl.begin()) {
+    tsl.enableAutoRange(true);
+    tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);
+  } else {
+    isLightSensorDetected = false;
+  }
 }
 
 void setup() {
@@ -91,16 +165,17 @@ void setup() {
   Serial.println(F("DHTxx test!"));
   Wire.begin();
 
+  configureLightSensor();
   dht.begin();
   lcd.init();
   lcd.backlight();
 
   // ido beallitasa
-  rtc.setYear(25);
+  /* rtc.setYear(25);
   rtc.setMonth(10);
   rtc.setDoW(3);
   rtc.setHour(21);
-  rtc.setMinute(56);
+  rtc.setMinute(56); */
 
  
   // custom karakterek letrehozasa
@@ -134,22 +209,40 @@ void printDayOrNight(uint8_t hour, uint8_t minute) {
   printWithZero(minute);
   lcd.print(" ");
 
-  if (hour >= 6 && hour <=22) {
+  if (isItDay) {
     lcd.printByte(1); // nappal karaktere
   } else {
     lcd.printByte(0); // ejszaka karaketre
   }
 }
 
+void printLuminosity() {
+  lcd.print("Light: ");
+
+  if (luminosity <= 0) {
+    lcd.print("Bright");
+  } else if (luminosity == 1) {
+    lcd.print("Okay");
+  } else {
+    lcd.print("Dark");
+  }
+}
+
 void loop() {
   // Wait a few seconds between measurements.
-  delay(500);
+  delay(2000);
   DateTime now = myRTC.now();
   uint16_t year = now.year();
   uint8_t month = now.month();
   uint8_t day = now.day();
   uint8_t hour = now.hour();
   uint8_t minute = now.minute();
+
+  checkDayTime(hour);
+  automaticModeLogic(now);
+
+  sensors_event_t event;
+  tsl.getEvent(&event);
 
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
@@ -182,20 +275,34 @@ void loop() {
 
   lcd.flush();
   lcd.clear();
+  
   printDate(year, month, day);
   printDayOrNight(hour, minute);
+  
   lcd.setCursor(0, 1);
   lcd.print(h);
   lcd.print(F("% "));
   lcd.print(t);
   lcd.printByte(4);
   lcd.print(F("C "));
+  
+  lcd.setCursor(0, 2);
+  if (event.light) {
+    printLuminosity();
+  } else {
+    lcd.print("Sensor overload.");
+  }
 
   buttonState = digitalRead(A0);
 
   if (buttonState != buttonOldState) {
     buttonOldState = buttonState;
-    lcd.setCursor(0, 2);
+    changeShutters(!isShutterUp);
+
+    lastButtonChangeTimestamp = now.unixtime();
+    isManual = true;
+
+    lcd.setCursor(0, 3);
     lcd.print(F("Switch"));
   }
 }
