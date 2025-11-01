26cdea7bcfff894bb638be7bd778afd4a9326c75
HVMetal <fonodizsolt20@gmail.com>
2025-11-02 01:38:54 +0200

    Fairly slowly updating webpage (maybe once every 5s), but it displays all required data. Also added data verification and recovery attempts.

--- Arduino_SPI/Arduino_SPI.ino
+++ Arduino_SPI/Arduino_SPI.ino
@@ -7,8 +7,8 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_TSL2561_U.h>
-#define DHTPIN A3  // Digital pin connected to the DHT sensor
 
+#define DHTPIN A3  // Digital pin connected to the DHT sensor
 #define DHTTYPE DHT11  // DHT 11
 
 #if defined(ARDUINO) && ARDUINO >= 100
@@ -17,26 +17,21 @@
 #define printByte(args) print(args, BYTE);
 #endif
 
-const int stepsPerRevolution = 200;
+const uint8_t STEPS_PER_REVOLUTION = 200;
+const uint8_t TO_AUTOMATIC_TIMER_S = 10;
+const uint8_t NORMAL_BRIGHTNESS_THRESHOLD = 100;
+const uint8_t TOO_BRIGHT_THRESHOLD = 1000;
 
 Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
 
 DHT dht(DHTPIN, DHTTYPE);
 RTC_DS3231 rtc;
-Stepper myStepper(stepsPerRevolution, 8, 6, 5, 4);
-
-const int automaticManualChangeDelay = 10;
-const int normalLightThreshold = 100;
-const int tooBrightThreshold = 1000;
+Stepper myStepper(STEPS_PER_REVOLUTION, 8, 6, 5, 4);
 
 uint32_t lastButtonChangeTimestamp = 0;
-int buttonOldState, buttonState;
-int luminosity = 1;    // <= 0 - too bright, 1 - okay, >= 2 - too dark
-int engineState = -1;  // <= 0 - off; >=1 - on
 
-bool isManual = false;
-bool isItDay;
-bool isShutterUp = true;
+int brightness = 1;    // <= 0 - too bright, 1 - okay, >= 2 - too dark
+uint8_t buttonOldState, buttonState;
 
 bool missingDHT = false;
 bool missingRTC = false;
@@ -45,12 +40,41 @@ bool missingTSL = false;
 SPISettings spi_settings(100000, MSBFIRST, SPI_MODE0);
 
 int pinDHT11 = A3;
-// SimpleDHT11 dht11(pinDHT11);
 
-volatile byte temperature;
-volatile byte humidity;
-volatile byte motorforgas;
+// The first four can be stored and sent in a single byte
+volatile bool isManual = false;
+volatile bool isDay = false;
+volatile bool isShutterUp = true;
 
+volatile int engineState = -1;  // <= 0 - off; >=1 - on
+
+/*
+  Bits:
+    0 - isModeManual
+    1 - isItDay
+    2 - isShutterUp
+    3 - engineState (0 if <= 0, 1 if >= 1)
+*/
+volatile byte flags = 0b00000000;
+
+// Luminosity needs a more sophisticated way to send over
+volatile uint16_t luminosity = 0;
+volatile bool highLumByte = false;
+
+volatile byte temperature = 0;
+volatile byte humidity = 0;
+
+volatile byte lastCommandByte = 0;
+volatile byte lastSentData = 0;
+
+// When true, subsequent requests will read and send data, or perform actions.
+// Otherwise they verify the correctness of the data from a previous command.
+volatile bool commandMode = true;
+
+// After setting the command mode, the next byte will be processed accordingly.
+// This is to avoid clash with certain special characters.
+// Its lifetime is one request.
+volatile bool commandModeSet = false;
 
 bool checkSensors() {
   float test = dht.readTemperature();
@@ -65,16 +89,22 @@ bool checkSensors() {
   return allGood;
 }
 
-void assessLuminosity(float light) {
+void assessBrightness(float light) {
+  luminosity = (uint32_t)light;
   if (light) {
-    if (light < normalLightThreshold) {
-      luminosity = 2;
-    } else if (light < tooBrightThreshold) {
-      luminosity = 1;
+    if (light < NORMAL_BRIGHTNESS_THRESHOLD) {
+      brightness = 2;
+    } else if (light < TOO_BRIGHT_THRESHOLD) {
+      brightness = 1;
     } else {
-      luminosity = 0;
+      brightness = 0;
     }
   }
+}
+
+void forceShutters() {
+  changeShutters(!isShutterUp);
+  isManual = true;
 }
 
 void assessButtonState(uint32_t unixtime) {
@@ -82,25 +112,24 @@ void assessButtonState(uint32_t unixtime) {
 
   if (buttonState != buttonOldState) {
     buttonOldState = buttonState;
-    changeShutters(!isShutterUp);
-
     lastButtonChangeTimestamp = unixtime;
-    isManual = true;
+
+    forceShutters();
   }
 }
 
 void checkDayTime(uint8_t hour) {
   if (hour >= 8 && hour <= 20) {
-    isItDay = true;
+    isDay = true;
   } else {
-    isItDay = false;
+    isDay = false;
   }
 }
 
 void canSwitchMode(uint32_t unixtime) {
   int timeDiff = abs(unixtime - lastButtonChangeTimestamp);
 
-  if (timeDiff >= automaticManualChangeDelay) {
+  if (timeDiff >= TO_AUTOMATIC_TIMER_S) {
     isManual = false;
   }
 }
@@ -114,7 +143,7 @@ void checkEngineState() {
   const int maxState = 5;
   if (engineState > 0 && engineState < maxState) {
     engineState++;
-    myStepper.step(stepsPerRevolution);
+    myStepper.step(STEPS_PER_REVOLUTION);
   }
   if (engineState == maxState) {
     engineState = 0;
@@ -123,11 +152,11 @@ void checkEngineState() {
 
 void automaticModeLogic(uint32_t unixtime) {
   if (!isManual) {
-    if (isItDay) {
-      if (luminosity <= 0 && isShutterUp) {
+    if (isDay) {
+      if (brightness <= 0 && isShutterUp) {
         changeShutters(false);
       }
-      if (luminosity >= 2 && !isShutterUp) {
+      if (brightness >= 2 && !isShutterUp) {
         changeShutters(true);
       }
     } else {
@@ -138,6 +167,13 @@ void automaticModeLogic(uint32_t unixtime) {
   } else {
     canSwitchMode(unixtime);
   }
+}
+
+void constructFlagByte() {
+  bitWrite(flags, 0, isManual ? 1 : 0);
+  bitWrite(flags, 1, isDay ? 1 : 0);
+  bitWrite(flags, 2, isShutterUp ? 1 : 0);
+  bitWrite(flags, 3, engineState > 0 ? 1 : 0);
 }
 
 void configureTSL() {
@@ -163,7 +199,8 @@ void configureDHT() {
 
 void configureRTC() {
   Wire.beginTransmission(0x68);
-  byte error = Wire.endTransmission();
+  uint8_t error = Wire.endTransmission();
+
   if (error == 0) {
     if (rtc.begin()) {
       rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
@@ -192,7 +229,6 @@ void setup(void) {
   //DHT11 adatai
   temperature = 1;
   humidity = 1;
-  motorforgas = 1;
 
   pinMode(A0, INPUT_PULLUP);
   buttonOldState = digitalRead(A0);
@@ -217,35 +253,38 @@ void printWithZero(uint8_t number) {
   Serial.print(number);
 }
 
-void printDate(uint16_t year, uint8_t month, uint8_t day) {
+void printDateTime(uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t minute) {
   Serial.print(year);
   Serial.print("/");
   printWithZero(month);
   Serial.print("/");
   printWithZero(day);
-  Serial.println(" ");
-}
-
-void printDayOrNight(uint8_t hour, uint8_t minute) {
+  Serial.print(" - ");
   printWithZero(hour);
   Serial.print(":");
   printWithZero(minute);
-  Serial.print(" ");
+  Serial.println();
 }
 
-void printLuminosity() {
-  Serial.print(F(" "));
-
+void printBrightness() {
   if (missingTSL) {
     Serial.println(F("No TSL"));
   } else {
-    if (luminosity <= 0) {
-      Serial.println(F("Bright"));
-    } else if (luminosity == 1) {
-      Serial.println(F("Okay"));
+    Serial.print(F("Lum: "));
+    Serial.print(luminosity);
+    Serial.print(F(" ("));
+    // Serial.print((highByte(luminosity) << 8) + lowByte(luminosity));
+    // Serial.print(F(") ("));
+    
+    if (brightness <= 0) {
+      Serial.print(F("Bright"));
+    } else if (brightness == 1) {
+      Serial.print(F("Okay"));
     } else {
-      Serial.println(F("Dark"));
+      Serial.print(F("Dark"));
     }
+
+    Serial.println(F(")"));
   }
 }
 
@@ -260,10 +299,11 @@ void printMode() {
 }
 
 void printShutterState() {
+  Serial.print(F("Shutter: "));
   if (isShutterUp) {
     Serial.println(F("UP"));
   } else {
-    Serial.println(F("UP"));
+    Serial.println(F("DOWN"));
   }
 }
 
@@ -278,10 +318,16 @@ void printEngineState() {
 }
 
 void printHumidityAndTemperature(float humidity, float temperature) {
-  Serial.print(humidity);
-  Serial.println(F("% "));
-  Serial.print(temperature);
-  Serial.println(F("C "));
+  if (!missingDHT) {
+    Serial.print(F("Humidity: "));
+    Serial.print(humidity);
+    Serial.println(F("% "));
+    Serial.print(F("Temperature: "));
+    Serial.print(temperature);
+    Serial.println(F("C "));
+  } else {
+    Serial.println(F("DHT sensor missing"));
+  }
 }
 
 void printSensorProblems() {
@@ -303,6 +349,8 @@ void printSensorProblems() {
 void loop() {
   // // DHT11 sampling rate is 1HZ.
   delay(1000);
+  bool allGood = checkSensors();
+  
   DateTime now = !missingRTC ? rtc.now() : DateTime(2000, 1, 1, 0, 0, 0);
   delay(50);
 
@@ -311,50 +359,83 @@ void loop() {
     tsl.getEvent(&event);
     delay(50);
     if (event.light) {
-      assessLuminosity(event.light);
+      assessBrightness(event.light);
     }
   }
-
-  bool allGood = checkSensors();
 
   humidity = !missingDHT ? dht.readHumidity() : 0;
   temperature = !missingDHT ? dht.readTemperature() : 20;
 
-  // checkDayTime(now.hour());
+  checkDayTime(now.hour());
   automaticModeLogic(now.unixtime());
   assessButtonState(now.unixtime());
+  checkEngineState();
+  constructFlagByte();
 
-  printDate(now.year(), now.month(), now.day());
-  printDayOrNight(now.hour(), now.minute());
-
-  if (missingDHT) {
-    Serial.println(F("DHT sensor missing"));
-  } else {
-    printHumidityAndTemperature(humidity, temperature);
-  }
-
+  printDateTime(now.year(), now.month(), now.day(), now.hour(), now.minute());
+  printHumidityAndTemperature(humidity, temperature);
   printMode();
   printShutterState();
-
-  printLuminosity();
+  printBrightness();
   printEngineState();
-  checkEngineState();
+
   Serial.println();
 }
 
-//SPI interrupt routine
-//ide akkor jon be ha kapott egy byte-ot
+// SPI interrupt routine
 ISR(SPI_STC_vect) {
+  byte c = SPDR;
 
-  //kiolvassuk a kapott karaktert
-  char c = SPDR;
+  if (!commandModeSet) {
+    commandMode = true;   // default
+    SPDR = 0xFF;
+    
+    if (c == '?') {
+      commandMode = false;
+    }
+      
+    commandModeSet = true;
+  } else {
+    if (commandMode) {
+      sendData(c);
+    } else {
+      verifyData(c);
+    }
 
-  if (c == 't')
-    SPDR = temperature;
-  else if (c == 'h')
-    SPDR = humidity;
-  else if (c == '0')
-    motorforgas = 0;
-  else if (c == 'm')
-    motorforgas = 1;
+    commandModeSet = false;
+  }
+}
+
+byte getLuminosityByte(bool highLumByte) {
+  return highLumByte ? highByte(luminosity) : lowByte(luminosity);
+}
+
+void sendData(byte c) {
+  switch (c) {
+    case 'b': SPDR = flags; break;
+    case 'f': {
+      SPDR = getLuminosityByte(highLumByte);
+      highLumByte = !highLumByte;
+      break;
+    }
+    case 'h': SPDR = temperature; break;
+    case 'e': SPDR = humidity; break;
+    case 'm': forceShutters(); break;
+    default: return;
+  }
+
+  lastSentData = SPDR;
+  lastCommandByte = c;
+}
+
+void verifyData(byte c) {
+  if (c != lastSentData) {
+    if (lastCommandByte == 'f') {
+      SPDR = getLuminosityByte(!highLumByte);
+    } else {
+      sendData(lastCommandByte);
+    }
+  } else {
+    SPDR = c;
+  }
 }
--- NodeMCU_weboldal/NodeMCU_weboldal.ino
+++ NodeMCU_weboldal/NodeMCU_weboldal.ino
@@ -1,16 +1,32 @@
 #include <ESP8266WiFi.h>
 #include <SPI.h>
 
-byte temperature;
-byte humidity;
-byte motorforgas;
+const uint8_t COMMAND_QUEUE_MAX_SIZE = 128;
+byte commandQueue[128];
+uint8_t currentQueueSize = 0;
+
+bool isManual = false;
+bool isDay = false;
+bool isShutterUp = false;
+bool isEngineOn = false;
+
+uint16_t luminosity = 0;
+byte lumLowerByte = 0, lumHigherByte = 0;
+
+byte flags = 0;
+byte temperature = 0;
+byte humidity = 0;
+
+byte resentData = 0;
+bool isDataError = false;
 
 SPISettings spi_settings(100000, MSBFIRST, SPI_MODE0);
 
-const char* ssid = "-";
-const char* password = "-";
+const char* ssid = "AndroidAP1571";
+const char* password = "mbqw1601";
 
-const int SPI_POLL_INTERVAL = 2000;
+const byte MAX_SPI_RETRIES = 3;
+const int SPI_POLL_INTERVAL = 1500;
 const int CLIENT_AVAILABILITY_TIMEOUT = 2000;
 unsigned long last_SPI_poll = 0;
 
@@ -52,77 +68,191 @@ void setup() {
   Serial.println("/");
 }
 
-void executeTransaction() {
+void resyncPoll() {
+  SPI.transfer('!');
+}
+
+bool attemptPolling(byte command, byte *data) {
+  byte sync = 0;
+  // Tell the slave to enter data sending mode.
+  SPI.transfer('!');
+
+  // Send the command.
+  sync = SPI.transfer(command);
+  if (sync != 0xFF) return false;
+
+  // Read the received data and tell the slave to enter data verifying mode.
+  byte receivedData = SPI.transfer('?');
+
+  // Re-send the received data to verify it.
+  sync = SPI.transfer(receivedData);
+  if (sync != 0xFF) return false;
+
+  byte resentData = SPI.transfer('!');
+  if (resentData != receivedData) return false;
+
+  // Reset slave to command setting mode.
+  sync = SPI.transfer(0x00);
+  if (sync != 0xFF) return false;
+
+  *data = receivedData;
+  return true;
+}
+
+void poll(byte command, byte *data) {
+  for (byte attempt = 0; attempt < MAX_SPI_RETRIES; ++attempt) {
+    if (attemptPolling(command, data)) {
+      return;
+    }
+
+    Serial.print(F("Retrying SPI command "));
+    Serial.print(command);
+    Serial.print(F(" (attempt "));
+    Serial.print(attempt);
+    Serial.println(F(")"));
+
+    delay(10);
+    resyncPoll();
+    delay(5);
+  }
+
+  Serial.print(F("SPI command failed after "));
+  Serial.print(MAX_SPI_RETRIES);
+  Serial.print(F(" attempts."));
+
+  *data = 0xFF;
+}
+
+void setLuminosity() {
+  luminosity = ((uint16_t)lumHigherByte << 8) | lumLowerByte;
+}
+
+void setBooleans() {
+  isManual = bitRead(flags, 0);
+  isDay = bitRead(flags, 1);
+  isShutterUp = bitRead(flags, 2);
+  isEngineOn = bitRead(flags, 3);
+}
+
+void executePolling() {
+  isDataError = false;
   SPI.beginTransaction(spi_settings);
   
-  SPI.transfer('t');
-  temperature = SPI.transfer('h');
-  humidity = SPI.transfer(0x00);
+  poll('b', &flags);
+  poll('h', &temperature);
+  poll('e', &humidity);
+  poll('f', &lumLowerByte);
+  poll('f', &lumHigherByte);
 
   SPI.endTransaction();
+
+  if (!isDataError) {
+    setLuminosity();
+    setBooleans();
+  }
+}
+
+void executeQueuedCommands() {
+
 }
 
 void mainPageScripts(WiFiClient &client) {
-  client.println("<script>");
-  client.println("function updateData() {");
-  client.println("  fetch('/data')");
-  client.println("    .then(response => response.json())");
-  client.println("    .then(data => {");
-  client.println("        document.getElementById('temp').textContent = data.temperature;");
-  client.println("        document.getElementById('hum').textContent = data.humidity;");
-  client.println("    })");
-  client.println("    .catch(error => {");
-  client.println("        console.log(\"Failed to fetch data: \");");
-  client.println("        console.log(error);");
-  client.println("        console.log();");
-  client.println("    });");
-  client.println("}");
-  client.println("setInterval(updateData, 2000);");
-  client.println("updateData();");
-  client.println("</script>");
+  client.println(F("<script>"));
+  client.println(F("function updateData() {"));
+  client.println(F("  fetch('/data')"));
+  client.println(F("    .then(response => response.json())"));
+  client.println(F("    .then(data => {"));
+  client.println(F("        document.getElementById('temp').textContent = data.temperature;"));
+  client.println(F("        document.getElementById('hum').textContent = data.humidity;"));
+  client.println(F("        document.getElementById('lum').textContent = data.luminosity"));
+  client.println(F("        document.getElementById('daynight').textContent = data.day ? \"DAY\" : \"NIGHT\";"));
+  client.println(F("        document.getElementById('sysmode').textContent = data.manual ? \"MANUAL\" : \"AUTOMATIC\";"));
+  client.println(F("        document.getElementById('shutterstate').textContent = data.shutter ? \"UP\" : \"DOWN\";"));
+  client.println(F("        document.getElementById('enginestate').textContent = data.engine ? \"ON\" : \"OFF\";"));
+  client.println(F("    })"));
+  client.println(F("    .catch(error => {"));
+  client.println(F("        console.log(\"Failed to fetch data: \", error);"));
+  client.println(F("    })"));
+  client.println(F("    .finally(() => {"));
+  client.println(F("        setTimeout(updateData, 2000);"));
+  client.println(F("    });"));
+  client.println(F("}"));
+  client.println(F("updateData();"));
+  client.println(F("</script>"));
 }
 
 void mainPageBody(WiFiClient &client) {
-  client.println("<body>");
-  client.println("<h1>Temperature & Humidity</h1>");
-  client.println("<p>Temperature: <span id='temp'>--</span>°C</p>");
-  client.println("<p>Humidity: <span id='hum'>--</span>%</p>");
+  client.println(F("<body>"));
+  client.println(F("<h3>Sensor data:</h3>"));
+  client.println(F("<p>Temperature: <span id='temp'>--</span>°C</p>"));
+  client.println(F("<p>Humidity: <span id='hum'>--</span>%</p>"));
+  client.println(F("<p>Luminosity: <span id='lum'>--</span> lux</p>"));
+  client.println(F("<p>-- Day/Night: <span id='daynight'>--</span></p>"));
+  client.println(F("<p>-- System mode: <span id='sysmode'>--</span></p>"));
+  client.println(F("<p>-- Shutter: <span id='shutterstate'>--</span></p>"));
+  client.println(F("<p>-- Engine: <span id='enginestate'>--</span></p>"));
 
   mainPageScripts(client);
-  client.println("</body>");
+
+  client.println(F("</body>"));
 }
 
 void mainPageHead(WiFiClient &client) {
-  client.println("<head>");
-  client.println("<title>Sensor Page</title>");
-  client.println("<meta charset=\"UTF-8\">");
-  client.println("</head>");
+  client.println(F("<head>"));
+  client.println(F("<title>Sensor Page</title>"));
+  client.println(F("<meta charset=\"UTF-8\">"));
+  client.println(F("</head>"));
 }
 
 void mainPageResponse(WiFiClient &client) {
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
+  client.println(F("HTTP/1.1 200 OK"));
+  client.println(F("Content-Type: text/html"));
+  client.println(F("Connection: close"));
+  client.println(F("Access-Control-Allow-Origin: *"));
   client.println();
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>");
+  client.println(F("<!DOCTYPE HTML>"));
+  client.println(F("<html>"));
+
   mainPageHead(client);
   mainPageBody(client);
-  client.println("</html>");
+  
+  client.println(F("</html>"));
   client.flush();
-  delay(5);
+}
+
+String createDataJson() {
+  String json = "{\"temperature\":";
+  json += temperature;
+  json += ",\"humidity\":";
+  json += humidity;
+  json += ",\"luminosity\":";
+  json += luminosity;
+  json += ",\"manual\":";
+  json += isManual;
+  json += ",\"day\":";
+  json += isDay;
+  json += ",\"shutter\":";
+  json += isShutterUp;
+  json += ",\"engine\":";
+  json += isEngineOn;
+  json += "}";
+
+  return json;
 }
 
 void dataRequestResponse(WiFiClient &client) {
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: application/json");
+  String json = createDataJson();
+  Serial.println(json);
+
+  client.println(F("HTTP/1.1 200 OK"));
+  client.println(F("Content-Type: application/json"));
+  client.println(F("Connection: close"));
+  client.print(F("Content-Length: "));
+  client.println(json.length());
+  client.println(F("Access-Control-Allow-Origin: *"));
   client.println();
-  client.print("{\"temperature\":\"");
-  client.print(temperature);
-  client.print("\",\"humidity\":\"");
-  client.print(humidity);
-  client.println("\"}");
+  client.println(json);
   client.flush();
-  delay(5);
 }
 
 void webClientResponse(WiFiClient &client, String request) {
@@ -134,45 +264,60 @@ void webClientResponse(WiFiClient &client, String request) {
   mainPageResponse(client);
 }
 
-void pollSPI() {
-  if (millis() - last_SPI_poll > SPI_POLL_INTERVAL) {
-    last_SPI_poll = millis();
-    executeTransaction();
-  }
-}
+String readClientRequest(WiFiClient &client, int timeout = 2000) {
+  unsigned long start = millis();
+  String request = "";
 
-String readClientRequest(WiFiClient &client) {
-  String request = client.readStringUntil('\r');
-  Serial.println(request);
-  client.flush();
+  while(client.connected() && millis() - start < timeout) {
+    while(client.available()) {
+      char c = client.read();
+      request += c;
+
+      if (request.endsWith("\r\n\r\n")) return request;
+    }
+  }
   return request;
 }
 
+void pollSPI() {
+  if (millis() - last_SPI_poll > SPI_POLL_INTERVAL) {
+    last_SPI_poll = millis();
+    executePolling();
+  }
+  
+  executeQueuedCommands();
+}
+
 void handleClient(WiFiClient &client) {
-  Serial.println("New client.");
+  if (client) {
+    Serial.println(F("New client."));
 
-  unsigned long timeout_timer = millis();
-  while (!client.available() && millis() - timeout_timer < CLIENT_AVAILABILITY_TIMEOUT) {
-    delay(1);
-  }
-  if (!client.available()) {
-    Serial.println("Client timed out. Stopping client...");
+    unsigned long currentTime = millis();
+    unsigned long timeoutTimer = currentTime;
+
+    while (client.connected() && currentTime - timeoutTimer < CLIENT_AVAILABILITY_TIMEOUT) {
+      currentTime = millis();
+      if (client.available()) {
+        String request = readClientRequest(client);
+        if (request.length()) {
+          webClientResponse(client, request);
+          break;
+        }
+      }
+    }
+    if (currentTime - timeoutTimer >= CLIENT_AVAILABILITY_TIMEOUT) {
+      Serial.println(F("Client timed out."));
+    }
+
     client.stop();
-    return;
+    Serial.println(F("Client disconnected."));
+    Serial.println();
   }
-
-  String request = readClientRequest(client);
-  webClientResponse(client, request);
-
-  Serial.println("Client disconnected.");
-  Serial.println();
 }
 
 void loop() {
   pollSPI();
 
   WiFiClient client = server.available();
-  if (!client) return;
-  
   handleClient(client);
 }
